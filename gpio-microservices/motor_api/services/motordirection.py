adcf6164e18c94b4c4eba673df04c63eb7372416
sentairanger <ed2point0@gmail.com>
2021-08-26 23:54:37 -0600

    Create motordirection.py

---
+++ gpio-microservices/motor_api/services/motordirection.py
@@ -0,0 +1,8 @@
+def get_motordirection():
+    return [
+        {"id": 1, "direction": "forward"},
+        {"id": 2, "direction": "backward"},
+        {"id": 3, "direction": "left"},
+        {"id": 4, "direction": "right"},
+        {"id": 5, "direction": "stop"}
+    ]
